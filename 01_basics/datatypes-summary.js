652be7582220a2a01c63f7ce24f4437b9cb837e1
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-06-10 11:41:20 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,31 +3,31 @@
 // 7 types : string,Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
-const scoreValue = 100.3
+const scoreValue = 100.3//dattype=number
 
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
 const id = Symbol('123')
-const anotherId = Symbol('123')
+const anotherId = Symbol('123')//datatype=symbol
 
-console.log(id === anotherId );
+// console.log(id === anotherId );
 
-const bigNumber = 345568712397654443456778788n
+const bigNumber = 345568712397654443456778788n// bigNumber datatype->bigint
 
 // Reference (Non primitive)
 
 // Array, Object, Function
 
-const heros = ["shaktiman", "naagraj", "doga"]
+const heros = ["shaktiman", "naagraj", "doga"]//heros -> datatype=object
 let myobj = {
     name : "aryan",
     age : 20,
 }
 
 const myFunction = function(){
-    console.log("hello world");
+    console.log("hello world");//datatype=function
 }
 
-console.log(typeof bigNumber );
+console.log(typeof  anotherId );
