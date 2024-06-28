39a358377e4a8be6fe1f68d0839a42621d7a5058
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-06-28 15:28:02 +0000

    functions 2nd part

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,38 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("aryan"));
-console.log(loginUserMessage("Aryan"));
+// console.log(loginUserMessage("Aryan"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 5000));
+
+
+const user = {
+    username: "aryan",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username: "sam",
+        price: 399
+    }
+)
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
