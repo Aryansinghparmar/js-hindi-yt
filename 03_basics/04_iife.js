3665800a4d332aa73c46d4864dd9fafc7b8f044f
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-01 13:21:50 +0000

    IIFE in JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immmediately Invoked Function Expressions (IIFE)
+
+(function chai () {
+    // named IIFE
+    console.log(`DB CONNECTED`);
+}) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+}  ) ('aaryan')
