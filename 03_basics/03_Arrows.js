b199c8dfbf36865b6f57480c1f69cfbffba235e7
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-01 12:50:44 +0000

    Arrows in JS

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -4,5 +4,50 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
+        console.log(this);
     }
+
 }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai() {
+//     let username = "aaryan"
+//     console.log(this.username);  
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "aaryan"
+//     console.log(this.username); 
+// }
+
+const chai =  () => {
+    let username = "aaryan"
+    console.log(this); 
+}
+
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "aaryan"})
+
+console.log(addTwo(3, 4));
+
+// const myArray = [2, 4, 53, 2334]
+
+// myArray.forEach()
