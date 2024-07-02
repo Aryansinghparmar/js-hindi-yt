c50f6468c4e09a424ef4a27b702699132258b10d
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-02 05:27:09 +0000

    contro; flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if( temperature === 40 ) {
+//  console.log("less than 50");
+// } else {
+//     console.log("temprature is more than 50");
+// }
+// console.log("executed");
+
+// const score = 200
+
+// if(score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+
