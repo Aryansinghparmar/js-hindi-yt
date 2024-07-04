680ca3465c461386f06023b9297b95354cc1e559
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-04 07:22:56 +0000

    control_flow part1

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -20,4 +20,35 @@
 // console.log(`User power: ${power}`);
 
 
+const balance = 1000
+
+// if (balance > 500) console.log("text"),console.log("text2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLoggedIn && debitCard && 2==="2") {
+    console.log("allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail ) {
+    console.log("user logged in");
+}
 
