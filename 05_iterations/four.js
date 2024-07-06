4e1f36e8290434eb2f0b72c8fd851c1b1117b293
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-06 05:13:49 +0000

    for in loop for arrays in JS

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+  // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programmming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programmming) {
+ //   console.log(programmming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United state of america")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//  console.log(key);
+// }
